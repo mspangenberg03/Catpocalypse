07fe4ad0bfdc495a5f204f538d2fbfd440fd5f43
nate3323 <ncornelison24@gmail.com>
2024-01-06 10:31:49 -0500

    Updated LaserPointerTower
    Minor issue that was not fixed on Github desktop

--- Catpocalypse/Assets/Scripts/Towers/LaserPointerTower.cs
+++ Catpocalypse/Assets/Scripts/Towers/LaserPointerTower.cs
@@ -13,7 +13,8 @@ public class LaserPointerTower : Tower
     // Update is called once per frame
     void Update()
     {
-        if(currentTarget != null {
+        if (currentTarget != null )
+        {
 
         }
     }
