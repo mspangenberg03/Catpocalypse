e5a7f5183a53ace9a5b74f381add3aa3c0c06253
mspangenberg03 <mspangenberg@drury.edu>
2024-01-07 20:21:30 -0600

    New cats in progress

--- Catpocalypse/Assets/Scripts/Cat/LightCat.cs
+++ Catpocalypse/Assets/Scripts/Cat/LightCat.cs
@@ -4,30 +4,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-
-public class NormalCat : MonoBehaviour
+public class LightCat : MonoBehaviour
 {
-    // This event is static so we don't need to subscribe the money manager to every cat instance's OnCatDied event.
     public static event EventHandler OnCatDied;
 
 
     private int distraction = 0; //How distracted the cat is currently
-    private int distractionThreshold = 50; //The amount of distraction it takes to fully distract the cat
+    private int distractionThreshold = 15; //The amount of distraction it takes to fully distract the cat
     private bool isDistracted = false; // If the cat has been defeated or not.
     private Vector3 destination; //Where the cat is moving to
     //Rigidbody rb;//The RigidBody component
     private NavMeshAgent agent;
-    private float damageToPlayer = 2f; //How much health the cat takes from the player
-
-    private PlayerHealthManager healthManager;
-    //private PlayerHealthManager healthManager;
     // Start is called before the first frame update
-    
+
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
-        healthManager = GameObject.FindGameObjectWithTag("Goal").gameObject.GetComponent<PlayerHealthManager>();
-        
+
     }
 
     // Update is called once per frame
@@ -43,7 +36,7 @@ public class NormalCat : MonoBehaviour
     private void Distracted()
     {
         isDistracted = true;
-        
+
     }
     //I am intending this function to be called from either the tower or the projectile that the tower fires
     public void DistractCat(int distractionValue)
@@ -56,14 +49,14 @@ public class NormalCat : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        
+
         if (other.gameObject.tag == "CatSpawnPoint" && isDistracted)
         {
             KillCat();
         }
-        if(other.gameObject.tag == "Goal")
+        if (other.gameObject.tag == "Goal")
         {
-            healthManager.TakeDamage(damageToPlayer);
+            //TODO: Health
             KillCat();
         }
     }
