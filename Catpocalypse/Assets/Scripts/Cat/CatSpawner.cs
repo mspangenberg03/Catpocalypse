70d45ea554c41a449628364429997f0aa9e4d789
mspangenberg03 <mspangenberg@drury.edu>
2024-01-07 22:18:15 -0600

    Changed spawner and slowed down Heavy cat

--- Catpocalypse/Assets/Scripts/Cat/CatSpawner.cs
+++ Catpocalypse/Assets/Scripts/Cat/CatSpawner.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -10,8 +11,11 @@ public class CatSpawner : MonoBehaviour
     private Transform spawnPoint1;
 
     [SerializeField] private GameObject normalCat;
+    [SerializeField] private GameObject heavyCat;
+    [SerializeField] private GameObject lightCat;
     [SerializeField] private Button startWaveButton;
     [SerializeField] private PlayerHealthManager healthManager;
+    private List<GameObject> catsToSpawn;
 
     private int waveCount = 0;
     private int catsInWave = 5;
@@ -20,7 +24,10 @@ public class CatSpawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //StartCoroutine(Spawner());
+        catsToSpawn = new List<GameObject>();
+        catsToSpawn.Add(normalCat);
+        catsToSpawn.Add(lightCat);
+        catsToSpawn.Add(heavyCat);
     }
 
     // Update is called once per frame
@@ -56,8 +63,9 @@ public class CatSpawner : MonoBehaviour
         
         for (int i = 0; i < catsInWave; i++)
         {
-            
-            Instantiate(normalCat, spawnPoint1);
+            int index = Random.Range(0,3);
+            GameObject cat = catsToSpawn[index];
+            Instantiate(cat, spawnPoint1);
             yield return new WaitForSeconds(2f);
         }
         
