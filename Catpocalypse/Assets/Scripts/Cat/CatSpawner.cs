6c131c4aac88ad8882e6c8fd9a12332ad58f12f1
Megafont <51346737+Megafont@users.noreply.github.com>
2024-01-10 21:07:12 -0600

    Created WaveManager class
    Level1 has two spawners, so I realized this is a problem. This class orchestrates them all. As a result, the StartWave button now targets WaveManager.StartNextWave in both levels. The WaveManager also lets you set in the inspector how many waves there are on the level.
    
    CatSpawner now has inspector setting for how many cats are in the first wave. This way different spawners can be set to start out differently different if we want.
    
    Added victory and defeat screens in both levels. Defeat screen is triggered by playerHealthManager. Victory screen is triggered by WaveManager.
    
    Fixed bug in camera dragging, and added option to invert drag direction.

--- Catpocalypse/Assets/Scripts/Cat/CatSpawner.cs
+++ Catpocalypse/Assets/Scripts/Cat/CatSpawner.cs
@@ -14,44 +14,33 @@ public class CatSpawner : MonoBehaviour
     [SerializeField, Tooltip("One possible spawn point for cats")]
     private Transform spawnPoint1;
 
+    [SerializeField] private int _CatsInFirstWave = 5;
+
     [SerializeField] private GameObject normalCat;
     [SerializeField] private GameObject heavyCat;
     [SerializeField] private GameObject lightCat;
-    [SerializeField] private Button startWaveButton;
-    [SerializeField] private PlayerHealthManager healthManager;
     [SerializeField] private PlayerCutenessManager cutenessManager;
 
     private List<GameObject> catsToSpawn;
 
-    private int waveCount = 0;
-    private int catsInWave = 5;
-    private int catsRemaining;
+
+    private int catsInWave;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        catsInWave = _CatsInFirstWave;
+
         catsToSpawn = new List<GameObject>();
         catsToSpawn.Add(normalCat);
         catsToSpawn.Add(lightCat);
         catsToSpawn.Add(heavyCat);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        GameObject[] cats = GameObject.FindGameObjectsWithTag("Cat");
-        if (cats.Length == 0 && healthManager.GetPlayerOutOfHealth() != true)
-        {
-            startWaveButton.enabled = true;
-        }                
-    }
     public void StartNextWave()
-    {
-        HUD.ShowWaveDisplay();
-
-        waveCount++;
-        if(waveCount == 1)
+    {       
+        if (WaveManager.Instance.WaveCount == 1)
         {
             StartCoroutine(Spawner());
         }
@@ -60,40 +49,28 @@ public class CatSpawner : MonoBehaviour
             catsInWave += 2;
             StartCoroutine(Spawner());
         }
-        startWaveButton.enabled = false;
     }
     public void StopSpawner()
     {
         StopCoroutine(Spawner());
-        HUD.HideWaveDisplay();
     }
     IEnumerator Spawner()
     {
-        CatBase.OnCatDied += OnCatDied;
-
-        catsRemaining = catsInWave;
-        
         for (int i = 0; i < catsInWave; i++)
         {
-            int index = Random.Range(0,3);
+            int index = Random.Range(0, 3);
             GameObject catPrefab = catsToSpawn[index];
-            
+
             GameObject cat = Instantiate(catPrefab, spawnPoint1);
-            
+
             CatBase catComponent = cat.GetComponent<CatBase>();
-            cutenessManager.AddCuteness(catComponent.Cuteness);            
+            cutenessManager.AddCuteness(catComponent.Cuteness);
 
             yield return new WaitForSeconds(2f);
         }
-        
+
     }
 
-    private void OnCatDied(object sender, EventArgs e)
-    {
-        catsRemaining--;
-        HUD.UpdateWaveInfoDisplay(catsInWave, catsRemaining);
 
-        if (catsRemaining < 1)
-            HUD.HideWaveDisplay();
-    }
+    public int CatsInWave { get { return _CatsInFirstWave; } }
 }
---
+++ Catpocalypse/Assets/Scripts/WaveManager.cs
@@ -0,0 +1,109 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+/// <summary>
+/// This class tracks how many cats are left across all spawners.
+/// </summary>
+public class WaveManager : MonoBehaviour
+{
+    public static WaveManager Instance;
+
+    [SerializeField] private int _TotalWavesInLevel = 5;
+
+    private List<CatSpawner> _CatSpawners;
+    private int _TotalCatsInWave;
+    private int _CatsRemainingInWave;
+
+    private int _WaveCount = 0;
+    private bool _WaveInProgress = false;
+
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.LogError("There is already a WaveManager in this scene. Self destructing!");
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+
+        CatBase.OnCatDied += OnCatDied;
+
+        HUD.HideWaveDisplay();
+    }
+
+    public void StartNextWave()
+    {
+        // Don't try to start a wave if one is already in progress.
+        if (IsWaveInProgress)
+            return;
+
+        _WaveInProgress = true;
+
+        _WaveCount++;
+
+
+        FindAllSpawners();
+
+        foreach (CatSpawner spawner in _CatSpawners)
+        {
+            spawner.StartNextWave();
+        }
+
+        CalculateTotalCatsInWave();
+
+        HUD.ShowWaveDisplay();
+        HUD.UpdateWaveInfoDisplay(_WaveCount, _CatsRemainingInWave);
+    }
+
+    public void StopAllSpawning()
+    {
+        foreach (CatSpawner spawner in _CatSpawners)
+        {
+            spawner.StopSpawner();
+        }
+    }
+
+    public void OnCatDied(object Sender, EventArgs e)
+    {
+        _CatsRemainingInWave--;
+
+        HUD.UpdateWaveInfoDisplay(_TotalCatsInWave, _CatsRemainingInWave);
+
+        if (_CatsRemainingInWave < 1)
+        {
+            HUD.HideWaveDisplay();
+            _WaveInProgress = false;
+
+            if (_WaveCount >= _TotalWavesInLevel && !FindObjectOfType<PlayerHealthManager>().IsPlayerDead)
+                FindObjectOfType<VictoryScreen>()?.Show();
+        }
+    }
+
+    private void CalculateTotalCatsInWave()
+    {
+        _TotalCatsInWave = 0;
+        foreach (CatSpawner spawner in _CatSpawners)
+        {
+            _TotalCatsInWave += spawner.CatsInWave;
+        }
+
+        _CatsRemainingInWave = _TotalCatsInWave;
+    }
+
+    private void FindAllSpawners()
+    {
+        _CatSpawners = FindObjectsByType<CatSpawner>(FindObjectsSortMode.None).ToList();
+    }
+
+    public int WaveCount { get { return _WaveCount; } }
+    public bool IsWaveInProgress { get { return _WaveInProgress; } }
+}
+
