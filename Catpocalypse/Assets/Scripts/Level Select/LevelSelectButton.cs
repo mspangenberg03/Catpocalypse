819de75ea7ff53cf7d85b97fa3f1c931c8fef49a
nate3323 <ncornelison24@gmail.com>
2025-07-08 10:24:19 -0400

    Comics First Push

--- Catpocalypse/Assets/Scripts/Level Select/LevelSelectButton.cs
+++ Catpocalypse/Assets/Scripts/Level Select/LevelSelectButton.cs
@@ -7,6 +7,12 @@ public class LevelSelectButton : MonoBehaviour
     // Start is called before the first frame update
     public void LoadScene(string sceneToLoad)
     {
-        SceneLoader_Async.LoadSceneAsync(sceneToLoad);
+        try
+        {
+            SceneLoader_Async.LoadSceneAsync("CutScene" + sceneToLoad);
+        } catch
+        {
+            SceneLoader_Async.LoadSceneAsync(sceneToLoad);
+        }
     }
 }
